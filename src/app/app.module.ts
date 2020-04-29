c78bf5973ae15ea492e78dae18c092ff6e9aa86f
Carson Neiss <swagmandergaming@gmail.com>
2020-04-29 16:16:39 -0500

    Added service and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ImageListComponent } from './image-list/image-list.component';
+import { AccountComponent } from './account/account.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageListComponent,
+    AccountComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
