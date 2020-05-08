d502c16396c5007472b4ee661430a8d3d46536db
Jack Hance <jack_e_hance@yahoo.com>
2020-05-07 19:08:22 -0500

    updated search

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Image} from '../image';
+import { ImageService } from '../image.service';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  imageList: Promise<Image[]>;
+  imageResults: Image[] = [];
+  showResults = false;
+  name = '';
 
-  constructor() { }
+  constructor(private imageService: ImageService) { }
 
+  async appendCollection(collection: Promise<Image[]>) {
+    (await collection).forEach(async element => {
+    this.imageResults.push(await element);
+    })
+  }
+
+  async searchImages() {
+    (await this.imageList).forEach(image => {
+      if (this.name === image.name) {
+        this.appendCollection(this.imageService.searchImages(image.tags));
+      }
+    })
+    this.showResults = !this.showResults;
+  }
+  
   ngOnInit(): void {
+    this.imageList = this.imageService.getImages();
+    this.showResults = false;
   }
 
 }
